ea7b3c11825850a6abb81b535138ee95f3766a73
hmohcine <61138229+hmohcine@users.noreply.github.com>
2021-07-06 14:31:52 +0000

    Deploying to staging from @ VitsolutionsMaroc/pixart.eu@d16e70c923d36191960eff128093f306bf726596 🚀

--- ftpdeploy/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  localBasePath: "/",
-  remoteBasePath: "/pixart-dev",
-  sync: [{ src: "/dist", dest: "/" }],
-};
--- src/config/accessKeys.js
+++
@@ -1,4 +0,0 @@
-export default {
-  mapBoxAccessToken:
-    "pk.eyJ1IjoiaG91ZGFzcyIsImEiOiJja2sxOHE5anAwb3phMzBuMmVuNG9ydzlvIn0.i74N4mdlw0AG4ufLHyQqbA"
-};
